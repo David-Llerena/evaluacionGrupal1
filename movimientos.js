87e650dbb0d413403586d71e7ab9fdfec72b9129
jhonGuaman <jg4470114@gmail.com>
2024-09-03 20:46:29 -0500

    Se realiza las funciones en movimientos.js y movimientos.htnl

--- movimientos.js
+++ movimientos.js
@@ -15,10 +15,20 @@ cargar=function(){
 
 filtrarMovimientos=function(numeroCuenta){
     let movimientosCuenta=[];
+    let cuentaRecuperada;
     //Se barre el arreglo de movimientos
     //En cada iteración, verifica si el numero de cuenta del movimiento es igual al que recibe como parametro
     //En caso de serlo, agrega la cuenta al arreglo movimientosCuenta
     //Invoca a mostrarMovimientos, pasándole como parámetro movimientosCuenta
+    movimientosCuenta
+    for(let i=0;i<movimientos.length;i++){
+        cuentaRecuperada=movimientos[i];
+        if(cuentaRecuperada.numeroCuenta==numeroCuenta){
+            movimientosCuenta.push(cuentaRecuperada);
+        }
+    }
+    mostrarMovimientos(movimientosCuenta); 
+
 }
 
 /*
@@ -30,6 +40,30 @@ mostrarMovimientos=function(misMovimientos){
     //Si ya pinta correctamente la tabla, hacer el siguiente cambio:
     //Si el tipo es D(DEBITO), mostrar el monto en negativo (multiplicar por -1)
     //Si el tipo es C(CREDITO), mostrar el monto en positivo (tal como está guardado)
+    let cmpTabla=document.getElementById("tablaMovimientos");
+        let movimiento;
+        let monto;
+        let contenidoTabla="<table><tr><th>Nº CUENTA</th><th>MONTO</th><th>TIPO</th></tr>"
+        for(let i=0;i<misMovimientos.length;i++){
+            movimiento=misMovimientos[i];
+            if(movimiento.tipo=="D"){
+                monto=-movimiento.monto;
+            }
+            else{
+                monto=movimiento.monto;
+            }
+            contenidoTabla+=
+            "<td>"+movimiento.numeroCuenta+"</td>"+
+            "<td>"+monto+"</td>"+
+            "<td>"+movimiento.tipo+"</td></tr>";
+        }
+        contenidoTabla+="</table>";
+        cmpTabla.innerHTML=contenidoTabla;
+}
+
+ejecutarMostrarMovimientos=function(){
+    let numeroCuenta=recuperarTexto("txtNumeroCuentaMovimientos");
+    filtrarMovimientos(numeroCuenta);
 }
 
 
